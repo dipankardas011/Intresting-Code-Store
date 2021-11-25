f1c473f8496987a1309574a8f310d70eb549a64a
DipankarDas <dipsonu10@hotmail.com>
2021-11-25 23:01:38 +0530

    Completed the B tree

--- ImplementationB+tree/main.c
+++ ImplementationB+tree/main.c
@@ -1,72 +1,191 @@
-/**
- * implementation of B-tree
- */
+// Searching a key on a B-tree in C
 
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
- // M degree so M-1 keys
-#define M 3
-typedef struct Node {
-    int keys[M-1];
-    struct Node* children[M];
-}BTree;
+#define MAX 3
+#define MIN 2
 
-BTree* allocateMemo() {
-    BTree* new = (BTree*)malloc(sizeof(BTree));
-    for (int i = 0; i < M; i++)
-    {
-        new->keys[i] = 0;
-        new->children[i] = NULL;
-    }
-    new->children[M - 1] = NULL;
-    return new;
+struct BTreeNode {
+  int val[MAX + 1], count;
+  struct BTreeNode* link[MAX + 1];
+};
+
+struct BTreeNode* root;
+
+// Create a node
+struct BTreeNode* createNode(int val, struct BTreeNode* child) {
+  struct BTreeNode* newNode;
+  newNode = (struct BTreeNode*)malloc(sizeof(struct BTreeNode));
+  newNode->val[1] = val;
+  newNode->count = 1;
+  newNode->link[0] = root;
+  newNode->link[1] = child;
+  return newNode;
 }
 
-/**
- * @return {nothing}
- * @def improve the display method
- */
-void displayB_Tree(BTree* root) {
-    if (!root) {
-        return;
-    }
-
-    for (size_t i = 0; i < M; i++)
-    {
-        printf("%d ", root->keys[i]);
-        displayB_Tree(root->children[i]);
-    }
-    displayB_Tree(root->children[M - 1]);
-    
+// Insert node
+void insertNode(int val, int pos, struct BTreeNode* node,
+  struct BTreeNode* child) {
+  int j = node->count;
+  while (j > pos) {
+    node->val[j + 1] = node->val[j];
+    node->link[j + 1] = node->link[j];
+    j--;
+  }
+  node->val[j + 1] = val;
+  node->link[j + 1] = child;
+  node->count++;
 }
 
-int main(int argc, char const *argv[])
-{
-    BTree* root = NULL;
-    int ch = 0;
-    do {
-        printf("Enter your choice\n");
-        printf("> 0. STOP\n");
-        printf("> 1. Insert key\n");
-        printf("> 2. Display\n");
-        printf("> 3. Delete key\n");
-        printf("-> ");
-        scanf("%d", &ch);
-        int kk;
-        switch (ch) {
-        case 1: {
-            break;
-            }
-            case 2: {
-                break;
-            }
-            case 3: {
-                break;
-            }
-        }
-    } while (ch);
-    return 0;
+// Split node
+void splitNode(int val, int* pval, int pos, struct BTreeNode* node, 
+                  struct BTreeNode* child, struct BTreeNode** newNode) {
+  int median, j;
+
+  if (pos > MIN)
+    median = MIN + 1;
+  else
+    median = MIN;
+
+
+
+  *newNode = (struct BTreeNode*)malloc(sizeof(struct BTreeNode));
+  j = median + 1;
+
+
+  while (j <= MAX) {
+    (*newNode)->val[j - median] = node->val[j];
+    (*newNode)->link[j - median] = node->link[j];
+    j++;
+  }
+
+
+  node->count = median;
+  (*newNode)->count = MAX - median;
+
+
+
+  if (pos <= MIN) {
+    insertNode(val, pos, node, child);
+  }
+  else {
+    insertNode(val, pos - median, *newNode, child);
+  }
+
+
+  *pval = node->val[node->count];
+  (*newNode)->link[0] = node->link[node->count];
+  node->count--;
+}
+
+// Set the value
+int setValue(int val, int* pval, struct BTreeNode* node, struct BTreeNode** child) {
+  int pos;
+  if (!node) {
+    *pval = val;
+    *child = NULL;
+    return 1;
+  }
+
+
+  // finding the position where it can be found
+  // keep in mind that indexing of keys[] are are 1....M not startting from 0 as
+  /*
+   * [0][1][2]
+   *  X  -  - (KEYS)
+   *   /   |  \
+   *  0    0   0(LINKS)
+   */
+
+  if (val < node->val[1]) {
+    // left most child
+    pos = 0;
+  }
+  else {
+    for (pos = node->count;(val < node->val[pos] && pos > 1); pos--);
+    // same element found
+    if (val == node->val[pos]) {
+      printf("Duplicates are not permitted\n");
+      return 0;
+    }
+  }
+
+  if (setValue(val, pval, node->link[pos], child)) {
+    if (node->count < MAX) {
+      insertNode(*pval, pos, node, *child);
+    }
+    else {
+      // nno of keys are == MAX where M = 3 and keys are 3
+      splitNode(*pval, pval, pos, node, *child, child);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+// Insert the value
+void insert(int val) {
+  int flag, i;
+  struct BTreeNode* child;
+
+  flag = setValue(val, &i, root, &child);
+  if (flag)
+    root = createNode(i, child);
+}
+
+// Search node
+void search(int val, int* pos, struct BTreeNode* myNode) {
+  if (!myNode) {
+    return;
+  }
+
+  if (val < myNode->val[1]) {
+    *pos = 0;
+  }
+  else {
+    for (*pos = myNode->count; (val < myNode->val[*pos] && *pos > 1); (*pos)--);
+
+    if (val == myNode->val[*pos]) {
+      printf("%d is found", val);
+      return;
+    }
+  }
+  search(val, pos, myNode->link[*pos]);
+
+  return;
+}
+
+// Traverse then nodes
+void traversal(struct BTreeNode* myNode) {
+  int i;
+  if (myNode) {
+    for (i = 0; i < myNode->count; i++) {
+      traversal(myNode->link[i]);
+      printf("%d ", myNode->val[i + 1]);
+    }
+    traversal(myNode->link[i]);
+  }
+}
+
+int main() {
+  int val, ch;
+
+  insert(8);
+  insert(9);
+  insert(10);
+  insert(11);
+  insert(15);
+  insert(16);
+  insert(17);
+  insert(18);
+  insert(20);
+  insert(23);
+
+  traversal(root);
+
+  printf("\n");
+  search(11, &ch, root);
+  printf("\n");
+  return 0;
 }
